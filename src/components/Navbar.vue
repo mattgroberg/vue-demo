941ab762993193eeb39f379646bf9e6ec3de39ec
Matt Groberg <matt.groberg@xdti.com>
2024-08-29 14:25:21 -0700

    init commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+    <nav class='Navbar'>
+        <router-link to='/'>Home</router-link>
+        <router-link to='/profile'>Profile</router-link>
+    </nav>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+    <div class='Home'>
+        Home
+    </div>
+</template>
+
+<style scoped>
+
+</style>
