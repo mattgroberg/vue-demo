35776dccb365c66a218bf042134eb649f8b9cd03
Matt Groberg <matt.groberg@xdti.com>
2024-10-14 16:12:08 -0700

    add mock api

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,4 @@
-import type { StorybookConfig } from '@storybook/vue3-vite'
+import type { StorybookConfig } from '@storybook/vue3-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -7,11 +7,12 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    'storybook-addon-mock'
   ],
   framework: {
     name: '@storybook/vue3-vite',
     options: {}
   }
-}
-export default config
+};
+export default config;
